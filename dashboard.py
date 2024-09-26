f186f6ef6c9e893766242f1aa3674d2f9af43833
Carlos Vidal <carvi03@liu.se>
2024-09-26 14:50:01 +0200

    - Add new format for Pessl sensors - New color palletes for every sensor group

--- dashboard.py
+++ dashboard.py
@@ -3,12 +3,12 @@ File Name: checkboxes.py
 Author: Carlos Vidal
 Date Created: 2024-09-25
 Last Modified: 2024-09-26
-Version: 1.4
+Version: 1.6
 Description:
     Use checkboxes to toogle datasets.
 
 Changes:
-- Color of 30cm same as 10cm but less opacity
+- Add new sensor formats from Pessl
 """
 
 from dash import Dash, dcc, html, Input, Output
@@ -21,16 +21,19 @@ app = Dash(__name__)
 # Load combined data from CSV
 # CSV format: Date, Value, Type, sensor_id
 # df = pd.read_csv('csv_files/combined_data.csv')
-rain_df = pd.read_csv('csv_files/Type_Rain.csv')
-Pessl_df = pd.read_csv('csv_files/Type_Pessl.csv')
-SM_df = pd.read_csv('csv_files/Type_SM.csv')
+rain_df = pd.read_csv('csv_files/private/rain.csv')
+Pessl_p1_df = pd.read_csv('csv_files/private/P1.csv')
+Pessl_px_df = pd.read_csv('csv_files/private/PX.csv')
+SM_df = pd.read_csv('csv_files/private/SM.csv')
 
 # List of colors
-colorlist = ['rgb(200,0,0)','rgb(0,200,0)','rgb(0,0,255)','rgb(127,127,0)','rgb(127,0,127)','rgb(0,127,127)','rgb(0,0,0)']
+color_sm = ['rgb(200,0,0)','rgb(0,200,0)','rgb(0,0,255)','rgb(127,127,0)','rgb(127,0,127)']
+color_p1 = ['rgb(0,51,0)','rgb(0,102,0)','rgb(0,153,0)','rgb(0,204,0)','rgb(0,255,0)','rgb(51,255,51)','rgb(102,255,102)','rgb(153,255,153)','rgb(204,255,204)']
+color_px = ['rgb(0,127,127)','rgb(0,0,0)','rgb(102,0,204)']
 
 # Define the app layout
 app.layout = html.Div([
-    html.H4('Soil Moisture and Rain Data v1.4'),
+    html.H4('Soil Moisture and Rain Data v1.6'),
     dcc.Graph(
         id="graph",
         style={'width': '80vw', 'height': '80vh'}
@@ -55,10 +58,10 @@ app.layout = html.Div([
 def update_chart(selected_groups):
     fig = go.Figure()
 
-    # Color list position
-    i = 0
     # Add SM data (lines)
     if 'SM' in selected_groups:
+        # Color list position
+        i = 0
         for sensor_id in SM_df['sensor_id'].unique():
             sensor_data = SM_df[SM_df['sensor_id'] == sensor_id]
 
@@ -71,7 +74,7 @@ def update_chart(selected_groups):
                     name=f'10cm {sensor_id}',
                     yaxis='y1',
                     visible='legendonly',  # Default visibility set to legendonly
-                    line=dict(color=colorlist[i]),
+                    line=dict(color=color_sm[i]),
                     opacity=0.5
                 )
             )
@@ -85,7 +88,7 @@ def update_chart(selected_groups):
                     name=f'30cm {sensor_id}',
                     yaxis='y1',
                     visible='legendonly',  # Default visibility set to legendonly
-                    line=dict(color=colorlist[i])
+                    line=dict(color=color_sm[i])
                 )
             )
 
@@ -94,19 +97,41 @@ def update_chart(selected_groups):
 
     # Add Pessl data (lines)
     if 'Pessl' in selected_groups:
-        # Pessl_df = df[df['Type'] == 'Pessl']
-        for sensor_id in Pessl_df['sensor_id'].unique():
-            sensor_data = Pessl_df[Pessl_df['sensor_id'] == sensor_id]
+        
+        # Add P1 (9 depths) data
+        for i in range(10, 91, 10):          
+            fig.add_trace(
+                go.Scatter(
+                    x=Pessl_p1_df['Date'],
+                    y=Pessl_p1_df[f'{i}cm'],
+                    mode='lines',
+                    name=f'{i}cm P1',
+                    yaxis='y1',
+                    visible='legendonly',  # Default visibility set to legendonly
+                    line=dict(color=color_p1[int(i/10) - 1])
+                )
+            )
+        
+        # Color list position
+        i = 0
+        
+        # Add all other Pessl sensors at 10cm
+        for sensor_id in Pessl_px_df['sensor_id'].unique():
+            sensor_data = Pessl_px_df[Pessl_px_df['sensor_id'] == sensor_id]
             fig.add_trace(
                 go.Scatter(
                     x=sensor_data['Date'],
-                    y=sensor_data['Value'],
+                    y=sensor_data['10cm'],
                     mode='lines',
-                    name=f'Soil Moisture {sensor_id}',
+                    name=f'10cm {sensor_id}',
                     yaxis='y1',
+                    line=dict(color=color_px[i]),
                     visible='legendonly'  # Default visibility set to legendonly
                 )
             )
+            
+            # Next color in the list
+            i = i+1
 
     # Add Rain data (bars)
     #if 'rain' in selected_groups:
@@ -120,7 +145,8 @@ def update_chart(selected_groups):
                 name=f'Rain {sensor_id}',
                 yaxis='y2',
                 marker_color='blue',
-                opacity=0.6
+                opacity=0.6,
+                width=14400000      # Adjust width in milliseconds (this is 14400000 ms = 4h minutes)
             )
         )
 
